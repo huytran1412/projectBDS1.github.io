c85ee16775e35031333e70c56f70be55c16f084f
huytran1412 <quochuytran1609@gmail.com>
2022-12-06 17:47:57 +0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    // Initialize the plugin
+    $('#my_popup').popup();
+});
+
+// Path: js\main.js
+$(document).ready(function() {
+    // Initialize the plugin
+    $('#my_popup').popup({
+        opacity: 0.3,
+        transition: '0.3s',
+        scrolllock: true
+    });
+});

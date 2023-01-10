07d8e167d21fbd78be5b0e489e0da33910356832
huytran1412 <109794594+huytran1412@users.noreply.github.com>
2023-01-10 07:32:42 +0700

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -2,13 +2,3 @@ $(document).ready(function() {
     // Initialize the plugin
     $('#my_popup').popup();
 });
-
-// Path: js\main.js
-$(document).ready(function() {
-    // Initialize the plugin
-    $('#my_popup').popup({
-        opacity: 0.3,
-        transition: '0.3s',
-        scrolllock: true
-    });
-});
